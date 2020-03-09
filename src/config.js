d5ce7ec51e508f3a163f459a3648f6d38a059c1e
Johanthan Moes <36646172+Johnman747@users.noreply.github.com>
2020-03-09 12:39:17 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,13 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBpF9dv_9QtmWzGVqntZ7dQos2x98gIYY",
-  authDomain: "pokedex-25358.firebaseapp.com",
-  databaseURL: "https://pokedex-25358.firebaseio.com",
-  projectId: "pokedex-25358",
-  storageBucket: "pokedex-25358.appspot.com",
-  messagingSenderId: "70726142589",
-  appId: "1:70726142589:web:3dab24d4fdb615be07bf14"
   };
 
 //initialize firebase
